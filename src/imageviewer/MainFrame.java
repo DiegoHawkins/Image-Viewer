fa40a728fff2a34f26304604f4a756c7c09ed72f
diego <diego@192.168.1.36>
2017-01-29 19:39:24 +0000

    Image Viewer v1

---
+++ src/imageviewer/MainFrame.java
@@ -0,0 +1,68 @@
+package imageviewer;
+
+import imageviewer.control.Command;
+import imageviewer.control.NextImageCommand;
+import imageviewer.ui.ImageDisplay;
+import imageviewer.ui.swing.SwingImageDisplay;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MainFrame extends JFrame{
+
+    private final Map<String,Command> commands = new HashMap<>();
+    private ImageDisplay imageDisplay;
+
+    public MainFrame() {
+        setTitle("Image Viewer");
+        setSize(800,600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        add(image());
+        add(toolbar(),BorderLayout.SOUTH);
+        setVisible(true);
+    }
+
+    void add(Command command) {
+        commands.put(command.name(), command);
+    }
+
+    public ImageDisplay getImageDisplay() {
+        return imageDisplay;
+    }
+    
+    private Component image() {
+        SwingImageDisplay swingImageDisplay = new SwingImageDisplay();
+        imageDisplay=swingImageDisplay;
+        return swingImageDisplay;
+    }
+
+    private Component toolbar() {
+        JPanel panel = new JPanel();
+        panel.add(button("prev"));
+        panel.add(button("next"));
+        return panel;
+    }
+    
+    private Component button(String name) {
+        JButton button = new JButton(name);
+        button.addActionListener(execute(name));
+        return button;
+    }
+    
+    private ActionListener execute(String name) {
+        return new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                commands.get(name).execute();
+            }
+        };
+    }    
+
+}
---
+++ src/imageviewer/persistence/files/FileImageLoader.java
@@ -0,0 +1,74 @@
+package imageviewer.persistence.files;
+
+import imageviewer.model.Image;
+import imageviewer.persistence.ImageLoader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FilenameFilter;
+import java.io.IOException;
+
+public class FileImageLoader implements ImageLoader {
+    
+    private static File[] files;
+    private final String[] ImageExtensions = new String[]{"jpg","png","bmp"};
+    
+    public FileImageLoader(String folder) {
+        files = new File(folder).listFiles(withImageExtension());
+    }
+
+    @Override
+    public Image load() {
+        return imageAt(0);
+    }
+    
+    private Image imageAt(int index) {
+        return new Image() {
+            @Override
+            public byte[] bitmap() {
+                try {
+                    FileInputStream is = new FileInputStream(files[index]);
+                    return read(is);
+                } catch (IOException ex) {
+                    return new byte[0];
+                }
+            }
+
+            private byte[] read(FileInputStream is) throws IOException {
+                byte[] buffer = new byte[4096];
+                ByteArrayOutputStream os = new ByteArrayOutputStream();
+                while(true) {
+                    int length = is.read(buffer);
+                    if(length<1) break;
+                    os.write(buffer, 0, length);
+                }
+                return os.toByteArray();
+            }
+
+            @Override
+            public Image next() {
+                if(index==files.length-1) return imageAt(0);
+                return imageAt(index+1);
+            }
+
+            @Override
+            public Image prev() {
+                if(index==0) return imageAt(files.length-1);
+                return imageAt(index-1);
+            }
+        };
+
+    }
+
+    private FilenameFilter withImageExtension() {
+        return new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                for (String extension : ImageExtensions)
+                    if(name.endsWith(extension)) return true;
+                return false;
+            }
+        };
+    }
+    
+}
